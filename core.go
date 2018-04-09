0d04d3dfcbe481772fd2090f899cefdfcbfa8595
Ravern Koh <ravernkoh@gmail.com>
2018-04-09 23:11:24 +0800

    Add conversion from termbox.Key to Key and add event loop

--- core.go
+++ core.go
@@ -1,9 +1,60 @@
 package ivo
 
+import (
+	"log"
+
+	termbox "github.com/nsf/termbox-go"
+)
+
 type Core struct {
+	Logger *log.Logger
 	KeyMap *KeyMap
+
+	data []byte // data of raw event polling
 }
 
 func NewCore() *Core {
-	return &Core{}
+	return &Core{
+		data: make([]byte, 32),
+	}
+}
+
+func (c *Core) Run() {
+	if err := termbox.Init(); err != nil {
+		c.Logger.Printf("termbox: could not initialize: %v", err)
+		return
+	}
+	defer termbox.Close()
+
+	termbox.SetInputMode(termbox.InputAlt | termbox.InputMouse)
+
+	for {
+		switch e := c.pollEvent(); e.Type {
+		case termbox.EventKey:
+			newKey(e)
+		case termbox.EventMouse:
+			break
+		}
+	}
+}
+
+func (c *Core) pollEvent() termbox.Event {
+	for {
+		switch e := termbox.PollRawEvent(c.data); e.Type {
+		case termbox.EventRaw:
+			data := c.data[:e.N]
+			e := termbox.ParseEvent(data)
+			if e.Type == termbox.EventNone {
+				e.Type = termbox.EventKey
+				e.Key = termbox.KeyEsc
+			}
+			return e
+		case termbox.EventResize:
+			break
+		case termbox.EventError:
+			c.Logger.Printf("termbox: polled error event: %v", e.Err)
+		default:
+			c.Logger.Print("termbox: polled unknwon event")
+		}
+	}
 }
--- key.go
+++ key.go
@@ -3,12 +3,168 @@ package ivo
 import (
 	"strconv"
 	"strings"
+
+	termbox "github.com/nsf/termbox-go"
 )
 
 type Key struct {
 	Code KeyCode
 	Rune rune
 	Mod  KeyMod
+}
+
+func newKey(e termbox.Event) Key {
+	var k Key
+	if e.Ch == 0 {
+		switch e.Key {
+		case termbox.KeyF1:
+			k.Code = KeyCodeF1
+		case termbox.KeyF2:
+			k.Code = KeyCodeF2
+		case termbox.KeyF3:
+			k.Code = KeyCodeF3
+		case termbox.KeyF4:
+			k.Code = KeyCodeF4
+		case termbox.KeyF5:
+			k.Code = KeyCodeF5
+		case termbox.KeyF6:
+			k.Code = KeyCodeF6
+		case termbox.KeyF7:
+			k.Code = KeyCodeF7
+		case termbox.KeyF8:
+			k.Code = KeyCodeF8
+		case termbox.KeyF9:
+			k.Code = KeyCodeF9
+		case termbox.KeyF10:
+			k.Code = KeyCodeF10
+		case termbox.KeyF11:
+			k.Code = KeyCodeF11
+		case termbox.KeyF12:
+			k.Code = KeyCodeF12
+		case termbox.KeyInsert:
+			k.Code = KeyCodeInsert
+		case termbox.KeyDelete:
+			k.Code = KeyCodeDelete
+		case termbox.KeyHome:
+			k.Code = KeyCodeHome
+		case termbox.KeyEnd:
+			k.Code = KeyCodeEnd
+		case termbox.KeyPgup:
+			k.Code = KeyCodePgup
+		case termbox.KeyPgdn:
+			k.Code = KeyCodePgdn
+		case termbox.KeyArrowUp:
+			k.Code = KeyCodeArrowUp
+		case termbox.KeyArrowDown:
+			k.Code = KeyCodeArrowDown
+		case termbox.KeyArrowLeft:
+			k.Code = KeyCodeArrowLeft
+		case termbox.KeyArrowRight:
+			k.Code = KeyCodeArrowRight
+		case termbox.KeyCtrlSpace:
+			k.Code = KeyCodeSpace
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlA:
+			k.Rune = 'a'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlB:
+			k.Rune = 'b'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlC:
+			k.Rune = 'c'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlD:
+			k.Rune = 'd'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlE:
+			k.Rune = 'e'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlF:
+			k.Rune = 'f'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlG:
+			k.Rune = 'g'
+			k.Mod = KeyModCtrl
+		case termbox.KeyBackspace:
+			k.Code = KeyCodeBackspace
+		case termbox.KeyTab:
+			k.Code = KeyCodeTab
+		case termbox.KeyCtrlJ:
+			k.Rune = 'j'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlK:
+			k.Rune = 'k'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlL:
+			k.Rune = 'l'
+			k.Mod = KeyModCtrl
+		case termbox.KeyEnter:
+			k.Code = KeyCodeEnter
+		case termbox.KeyCtrlN:
+			k.Rune = 'n'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlO:
+			k.Rune = 'o'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlP:
+			k.Rune = 'p'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlQ:
+			k.Rune = 'q'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlR:
+			k.Rune = 'r'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlS:
+			k.Rune = 's'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlT:
+			k.Rune = 't'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlU:
+			k.Rune = 'u'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlV:
+			k.Rune = 'v'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlW:
+			k.Rune = 'w'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlX:
+			k.Rune = 'x'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlY:
+			k.Rune = 'y'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrlZ:
+			k.Rune = 'z'
+			k.Mod = KeyModCtrl
+		case termbox.KeyEsc:
+			k.Code = KeyCodeEsc
+		case termbox.KeyCtrl4:
+			k.Rune = '4'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrl5:
+			k.Rune = '5'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrl6:
+			k.Rune = '6'
+			k.Mod = KeyModCtrl
+		case termbox.KeyCtrl7:
+			k.Rune = '7'
+			k.Mod = KeyModCtrl
+		case termbox.KeySpace:
+			k.Code = KeyCodeSpace
+		case termbox.KeyBackspace2:
+			k.Code = KeyCodeBackspace
+		}
+	} else {
+		k.Rune = e.Ch
+	}
+	if e.Mod&termbox.ModAlt != 0 {
+		k.Mod |= KeyModAlt
+	}
+	return k
 }
 
 // TODO improve the way this is hashed
