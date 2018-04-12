cac6102eb7b7473820cbf076911fb3ee6c3ddb9a
Ravern Koh <ravernkoh@gmail.com>
2018-04-12 11:41:10 +0800

    Add newMouse

--- mouse.go
+++ mouse.go
@@ -1,11 +1,36 @@
 package ivo
 
+import termbox "github.com/nsf/termbox-go"
+
 // Mouse is a mouse event.
 type Mouse struct {
 	// Action is the action that occured.
 	Action MouseAction
-	Col    int
-	Row    int
+
+	Col int // x coordinate
+	Row int // y coordinate
+}
+
+// newMouse creates a new Mouse based on the values found in the termbox.Event.
+func newMouse(e termbox.Event) Mouse {
+	var m Mouse
+	m.Col = e.MouseX
+	m.Row = e.MouseY
+	switch e.Key {
+	case termbox.MouseLeft:
+		m.Action = MouseButtonLeft
+	case termbox.MouseMiddle:
+		m.Action = MouseButtonMiddle
+	case termbox.MouseRight:
+		m.Action = MouseButtonRight
+	case termbox.MouseRelease:
+		m.Action = MouseButtonRelease
+	case termbox.MouseWheelUp:
+		m.Action = MouseWheelUp
+	case termbox.MouseWheelDown:
+		m.Action = MouseWheelDown
+	}
+	return m
 }
 
 // MouseAction represents a mouse button, scroll or release.
