2bf2521ed7a043d5762cd991767444ee94ae03bd
Ravern Koh <ravernkoh@gmail.com>
2018-04-12 22:11:32 +0800

    Add improved docs for Command

--- command.go
+++ command.go
@@ -1,6 +1,10 @@
 package ivo
 
 // Command is an arbituary command sent to buffers.
+//
+// Command is mainly used to communicate between buffers and for user commands
+// (e.g. through the command bar). Commands should be used only when necessary,
+// since they inherently break the type system.
 type Command struct {
 	Name    string
 	Payload map[string]interface{}
