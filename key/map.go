5d546dc37f743e0c9280b74e009dbbb67341d4c3
Ravern Koh <ravernkoh@gmail.com>
2018-04-16 17:08:59 +0800

    Add docs for key.Map

--- key/map.go
+++ key/map.go
@@ -2,21 +2,25 @@ package key
 
 import "ivoeditor.com/ivo"
 
+// Map is a map of keys to actions, with support for modes.
 type Map struct {
 	modes map[string]*node
 }
 
+// node forms an action tree with other nodes.
 type node struct {
 	children map[ivo.Key]*node
 	action   func(ivo.Context)
 }
 
+// NewMap creates an empty map.
 func NewMap() *Map {
 	return &Map{
 		modes: make(map[string]*node),
 	}
 }
 
+// Set sets the action for the given keys, for the specified mode.
 func (m *Map) Set(mode string, kk []ivo.Key, action func(ivo.Context)) {
 	node := m.mode(mode)
 	for _, k := range kk {
@@ -30,6 +34,7 @@ func (m *Map) Set(mode string, kk []ivo.Key, action func(ivo.Context)) {
 	node.action = action
 }
 
+// Get returns the action for the given keys, for the specified mode.
 func (m *Map) Get(mode string, kk []ivo.Key) (func(ivo.Context), bool, bool) {
 	node, ok := m.modes[mode]
 	if !ok {
@@ -45,6 +50,8 @@ func (m *Map) Get(mode string, kk []ivo.Key) (func(ivo.Context), bool, bool) {
 	return node.action, len(node.children) != 0, true
 }
 
+// mode returns the node for the specified mode, creating it if it
+// doesn't exist.
 func (m *Map) mode(mode string) *node {
 	node, ok := m.modes[mode]
 	if !ok {
@@ -54,6 +61,7 @@ func (m *Map) mode(mode string) *node {
 	return node
 }
 
+// newNode creates an empty node.
 func newNode() *node {
 	return &node{
 		children: make(map[ivo.Key]*node),
