2be1cd9f7a312eb4c599e4e21fe9a2064109241c
Ravern Koh <ravernkoh@gmail.com>
2018-04-19 14:55:40 +0800

    Add some package docs for root package

--- doc.go
+++ doc.go
@@ -1,2 +1,16 @@
-// Package ivo implements the core packages.
+// Package ivo implements core packages.
+//
+// The root package provides many of the core interfaces for
+// customizing and creating packages for Ivo. It also contains the
+// main event loop and the core rendering, which relies on termbox
+// to interact with the terminal. The other subpackages provide
+// common implementations or utilities that supplement these core
+// types to make custom packages easier to build.
+//
+// Window is the most important interface. It is considered the
+// core building block of Ivo, since custom packages are split by
+// the Window implementations they provide. The concept is that
+// the main loop will send events to a single Window. If more than
+// one Window is required, then Window multiplexers can be used to
+// split the screen.
 package ivo
