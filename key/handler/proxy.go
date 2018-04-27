f73f6eb3b5d2b799a5bdd05ad86fee3d6dddd2b8
Ravern Koh <ravernkoh@gmail.com>
2018-04-27 08:24:22 +0800

    Fix handler.Proxy

--- key/handler/proxy.go
+++ key/handler/proxy.go
@@ -2,9 +2,7 @@ package handler
 
 import "ivoeditor.com/ivo"
 
-// Prompt represents a handler for prompt-related actions.
-type Prompt interface {
-	Confirm(ivo.Context, []ivo.Key) // confirm prompt
-	Cancel(ivo.Context, []ivo.Key)  // cancel prompt
-	Raw(ivo.Context, []ivo.Key)     // insert raw runes
+// Proxy represents a handler for proxy-related actions.
+type Proxy interface {
+	Forward(ivo.Context, []ivo.Key) // forward raw keys
 }
